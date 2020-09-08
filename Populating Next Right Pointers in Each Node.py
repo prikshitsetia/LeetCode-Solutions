54c5fefd0faf924977e0f8ce5102941059fd8131
Prikshit <prikshitsetia1878@gmail.com>
2020-09-09 02:32:07 +0530

    Leetcode 116

---
+++ Populating Next Right Pointers in Each Node.py
@@ -0,0 +1,23 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if root == None:
+            return root
+        if root.left:
+            root.left.next = root.right
+            self.connect(root.left)
+        if root.right:
+            if root.next:
+                root.right.next = root.next.left
+            self.connect(root.right)
+        return root
